e0920b9028c595381d1ff900df625bd363d655bb
MoritaSuguru <morisug4645@gmail.com>
2017-06-28 16:04:34 +0900

    【シーン関係のスクリプトを追加】
    ・各シーンのGameManagerを追加しました。
    ・以下、そのスクリプトファイルを更新してください。

---
+++ Assets/Scripts/Scenes/Title/GameManager.cs
@@ -0,0 +1,22 @@
+﻿//担当：
+//概要：タイトルシーン全体の管理をします。
+//参考：
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z10.Title {
+	public class GameManager : MonoBehaviour {
+
+		// Use this for initialization
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
