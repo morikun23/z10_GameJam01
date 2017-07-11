e0e9ee92c04c810c26bafc8840d0ec2fa4b81c88
MoritaSuguru <morisug4645@gmail.com>
2017-07-11 17:06:10 +0900

    【ステージのスクリプトファイルを追加】
    ・ステージに関するプログラムを追加しました。

---
+++ Assets/Scripts/Stage/Stage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z10 {
+	public class Stage {
+
+		public const int FLOOR_COUNT = 3;
+		public const float FLOOR_HEIGHT = 180;
+		
+
+	}
+}
