8095b9727565b2d733d15f4620586ec1e9a3bc8e
MoritaSuguru <morisug4645@gmail.com>
2017-07-11 17:12:58 +0900

    【梯子の再追加】
    ・先のコミットで梯子の一部ファイルが追加しきれていなかったので追加しました。

---
+++ Assets/Scripts/Actors/Buddy/Player/LadderUser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z10 {
+	public class LadderUser : MonoBehaviour {
+
+		//梯子が使える状態か
+		private bool m_isUseful;
+
+		public virtual void OnTriggerEnter2D(Collider2D arg_colliderInfo) {
+			if(arg_colliderInfo.gameObject.tag == "Ladder")
+			m_isUseful = true;
+		}
+
+		public virtual void OnTriggerExit2D(Collider2D arg_colliderInfo) {
+			if(arg_colliderInfo.gameObject.tag == "Ladder")
+			m_isUseful = false;
+		}
+
+		/// <summary>
+		/// 梯子が使える状態かを調べる
+		/// </summary>
+		/// <returns></returns>
+		public bool IsLadderUseFul() {
+			return m_isUseful;
+		}
+	}
+}
