b7fb79dd9b22c4d435dcd7bfe9b9ce3388c029ca
MoritaSuguru <morisug4645@gmail.com>
2017-07-13 13:15:23 +0900

    【Stageスクリプトを編集】
    ・現在の階層を調べる関数を追加（実装途中）

--- Assets/Scripts/Stage/Stage.cs
+++ Assets/Scripts/Stage/Stage.cs
@@ -6,8 +6,19 @@ namespace Z10 {
 	public class Stage {
 
 		public const int FLOOR_COUNT = 3;
-		public const float FLOOR_HEIGHT = 180;
-		
+		public const float FLOOR_HEIGHT = 3;
 
+		//最低フロアの座標
+		private const float ONE_FLOOR_Y = -4.5f;
+
+		public static int GetCurrentFloor(float arg_vertical) {
+			for(int i = 1; i < FLOOR_COUNT; i++) {
+				//i階にいると仮定
+				//i階の床とi+1階の天井の間にいるかを調べる
+				//下層
+				float floor = ONE_FLOOR_Y + i;
+			}
+			return 0;
+		}
 	}
 }
