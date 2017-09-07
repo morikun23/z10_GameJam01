37b8016f426bf1f67f9309482c4b0a1b1454d9bd
oyama <oyameycry3024@gmail.com>
2017-09-07 23:31:03 +0900

    【音とちょっと修正】

--- Assets/Scripts/SandBox/Oyama/EnemyBreakTest.cs
+++ Assets/Scripts/SandBox/Oyama/EnemyBreakTest.cs
@@ -8,21 +8,25 @@ public class EnemyBreakTest : MonoBehaviour {
 
     int max = 40;
 
-    GameObject[] unkos = new GameObject[80];
+    GameObject[] unkos = new GameObject[120];
 
     public GameObject effect;
 
     ParticleSystem effect_ps;
+
+    public AudioClip sound;
+    AudioSource audio;
 
     int cnt = 0;
 
     // Use this for initialization
     void Start() {
 
+        audio = GetComponent<AudioSource>();
         effect_ps = effect.GetComponent<ParticleSystem>();
 
-        for (int j = 0; j < 6; j++) {
-            for (int i = 0; i < 10; i++)
+        for (int j = 0; j < 7; j++) {
+            for (int i = 0; i < 14; i++)
             {
                 unkos[cnt] = Instantiate(enemy, transform.position + new Vector3(i + 1, -j - 1, 0), Quaternion.identity);
                 cnt++;
@@ -37,10 +41,13 @@ public class EnemyBreakTest : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            audio.PlayOneShot(sound,1);
+            
+
             //放出量を上げる
             effect_ps.emission.SetBursts(new ParticleSystem.Burst[]{
                 new ParticleSystem.Burst(0, 0),
-                new ParticleSystem.Burst(0, (short)(5 + (cnt/2)))
+                new ParticleSystem.Burst(0, (short)(5 + (cnt/4)))
             });
             GameObject eff = Instantiate(effect, unkos[cnt].transform.position, Quaternion.identity);
             Destroy(unkos[cnt]);
