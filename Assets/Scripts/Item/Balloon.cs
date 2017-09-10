b1f177d5d4ff9deee9d0d32c47388485bb4eed73
MoritaSuguru <morisug4645@gmail.com>
2017-09-10 22:29:30 +0900

    【アイテム】基底クラスとサンプルを追加

---
+++ Assets/Scripts/Item/Balloon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z10 {
+	public class Balloon : Item {
+
+		// Use this for initialization
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		public override void Update() {
+			base.Update();
+			transform.position += Vector3.left * 0.03f;
+		}
+
+		protected override void OnGotten() {
+			AudioSource.PlayClipAtPoint(Resources.Load<AudioClip>("Audios/item") , transform.position);
+			base.OnGotten();
+		}
+	}
+}
---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Z10 {
+	public class Item : MonoBehaviour {
+
+		public virtual void Update() {
+
+		}
+
+		protected Player player;
+
+		[SerializeField]
+		protected GameObject effect;
+
+		protected virtual void OnGotten() {
+			GameObject emergedEffect = Instantiate(effect , transform.position , Quaternion.identity);
+			Destroy(this.gameObject);
+			Destroy(emergedEffect , 1.5f);
+		}
+
+		public void OnTriggerEnter2D(Collider2D collider) {
+			if (collider.gameObject.tag == "Player") {
+				player = collider.gameObject.GetComponent<Player>();
+				OnGotten();
+			}
+		}
+	}
+}
